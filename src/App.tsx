35ff2ed26efb332a29a7d936cf1fd74e1b9c401f
jenDevelopez <jen1992lo@gmail.com>
2023-06-30 12:38:42 +0200

    Ejercicio 1 terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { DivApp, DivCheckbox } from "./StyledComponents";
+import { data } from "./data";
+import { useState } from "react";
 
-export default function App() {
+export default function App(): JSX.Element {
+  const [checked, setChecked] = useState(new Array(data.length).fill(false));
+  const [total, setTotal] = useState(0);
+  const handleChange = (position: number) => {
+    const checkedState = checked.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setChecked(checkedState);
+
+    const totalPrice = checkedState.reduce((sum, curr, index) => {
+      if (curr === true) {
+        return sum + data[index].price;
+      }
+      return sum;
+    }, 0);
+    setTotal(totalPrice);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <DivApp>
+      {data.map(({ title, price }, index) => {
+        return (
+          <DivCheckbox key={index}>
+            <input
+              type="checkbox"
+              id={`checkbox-${index}`}
+              checked={checked[index]}
+              onChange={() => handleChange(index)}
+            />
+            <label htmlFor={`checkbox-${index}`}>
+              {title} {price}
+            </label>
+          </DivCheckbox>
+        );
+      })}
+      <div>{total}</div>
+    </DivApp>
+  );
 }
---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id:1,
+    title: 'Crear una pagina web',
+    price: 500
+  },
+  {
+    id:2,
+    title: 'Consultoría SEO',
+    price: 300
+  },
+  {
+    id:3,
+    title: 'Campaña de Google Ads',
+    price: 200
+  }
+]
